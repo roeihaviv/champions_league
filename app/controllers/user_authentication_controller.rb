bc96e891d450cffa7c862fa90da05a877fa00173
roeihaviv <rhaviv@chicagobooth.edu>
2022-05-27 15:42:23 +0000

    Champions League - sign in works

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -1,4 +1,6 @@
 class UserAuthenticationController < ApplicationController
+  # Uncomment line 3 in this file and line 5 in ApplicationController if you want to force users to sign in before any other actions.
+  # skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie] })
 
   def sign_in_form
     render({ :template => "user_authentication/sign_in.html.erb" })
@@ -36,11 +38,11 @@ class UserAuthenticationController < ApplicationController
 
   def create
     @user = User.new
-    @user.first_name = params.fetch("query_first_name")
-    @user.last_name = params.fetch("query_last_name")
+    @user.email = params.fetch("query_email")
     @user.password = params.fetch("query_password")
     @user.password_confirmation = params.fetch("query_password_confirmation")
-    @user.email = params.fetch("query_email")
+    @user.first_name = params.fetch("query_first_name")
+    @user.last_name = params.fetch("query_last_name")
 
     save_status = @user.save
 
@@ -59,11 +61,11 @@ class UserAuthenticationController < ApplicationController
 
   def update
     @user = @current_user
-    @user.first_name = params.fetch("query_first_name")
-    @user.last_name = params.fetch("query_last_name")
+    @user.email = params.fetch("query_email")
     @user.password = params.fetch("query_password")
     @user.password_confirmation = params.fetch("query_password_confirmation")
-    @user.email = params.fetch("query_email")
+    @user.first_name = params.fetch("query_first_name")
+    @user.last_name = params.fetch("query_last_name")
     
     if @user.valid?
       @user.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_193136) do
+ActiveRecord::Schema.define(version: 2022_05_27_154018) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "club_name"
@@ -56,6 +56,15 @@ ActiveRecord::Schema.define(version: 2022_05_26_193136) do
     t.integer "season_id"
     t.string "player_image"
     t.integer "club_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
